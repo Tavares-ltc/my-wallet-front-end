348dad20bdac6bb79ada53ad57f8d2631a28a96a
Lucas Tavares <l.tavaresc@hotmail.com>
2022-09-07 12:00:29 -0300

    Primeiro commit.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./HomePage";
+
+export default function App() {
+
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<HomePage type={'login'} />} />
+                </Routes>
+            </BrowserRouter>
+        </>
+    )
+}
---
+++ src/HomePage.js
@@ -0,0 +1,14 @@
+
+export default function HomePage({type}) {
+    if(type === 'login'){
+
+        return(
+            <h1>aaa</h1>
+            ) 
+        }
+    
+    return (
+        <>
+        </>
+    )
+}
