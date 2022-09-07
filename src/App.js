7f6d47eaa6e657320820b16949fa00cde5412145
Lucas Tavares <l.tavaresc@hotmail.com>
2022-09-07 15:22:30 -0300

    Tela de login e de cadastro criadas.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./HomePage";
+import styled from "styled-components";
 
 export default function App() {
 
     return (
         <>
+
+
             <BrowserRouter>
                 <Routes>
-                    <Route path='/' element={<HomePage type={'login'} />} />
+                    <Route path='/' element={<HomePage type={'sign-in'} />} />
+                    <Route path='/cadastro' element={<HomePage type={'sign-up'} />} />
                 </Routes>
             </BrowserRouter>
         </>
     )
 }
+
+
--- src/HomePage.js
+++ src/HomePage.js
@@ -1,14 +1,133 @@
+import styled from 'styled-components';
+import { useState } from "react";
+import { useNavigate } from 'react-router-dom';
 
-export default function HomePage({type}) {
-    if(type === 'login'){
+export default function HomePage({ type }) {
+    const [loginData, setLoginData] = useState({ email: '', password: '' })
+    const [signUpData, setSignUpData] = useState({name:'', email: '', password: ''})
+    const [hover, setHover] = useState(false)
+    const navigate = useNavigate()
 
-        return(
-            <h1>aaa</h1>
-            ) 
-        }
-    
+    if (type === 'sign-in') {
+
+        return (
+            <Page >
+                <h1 >My Wallet</h1>
+                <Forms onSubmit={sendData} hover={hover}>
+                    <form >
+                        <input type='email' placeholder='E-mail' name='email' onChange={handleLogin} required />
+                        <input type='password' placeholder='Senha' name='password' onChange={handleLogin} required />
+                        <button type='submit' value='entrar'> Entrar</button>
+                    </form>
+                    <p onMouseOver={() => setHover(!hover)}
+                        onMouseLeave={() => setHover(!hover)}
+                        onClick={() => navigate('/cadastro')}> Primeira vez? Cadastre-se!</p>
+                </Forms>
+            </Page>
+        )
+    }
+
     return (
-        <>
-        </>
+        <Page >
+            <h1 >My Wallet</h1>
+            <Forms onSubmit={sendData} hover={hover}>
+                <form >
+                    <input type='text' placeholder='Nome' name='name'onChange={handleSignUp} required/>
+                    <input type='email' placeholder='E-mail' name='email' onChange={handleSignUp} required />
+                    <input type='password' placeholder='Senha' name='password' onChange={handleSignUp} required />
+                    <input type='password' placeholder='Confirme a senha' name='password' onChange={handleSignUp} required />
+                    <button type='submit' value='entrar'> Entrar </button>
+                </form>
+                <p onMouseOver={() => setHover(!hover)}
+                    onMouseLeave={() => setHover(!hover)}
+                    onClick={() => navigate('/')}> Já tem cadastro? Faça o login!</p>
+            </Forms>
+        </Page>
     )
+    function sendData(event) {
+        event.preventDefault();
+        if(type === 'sign-in') {
+         console.log(loginData)
+        //    navigate('/mywallet')
+    } else {
+            //verificar se as senhas são iguais
+            console.log(signUpData)
+        }
+    }
+
+    function handleSignUp (event) {
+        setSignUpData({
+            ...signUpData,
+            [event.target.name]: event.target.value
+        })
+    }
+
+    function handleLogin(event) {
+        setLoginData({
+            ...loginData,
+            [event.target.name]: event.target.value
+        })
+    }
+
 }
+
+
+const Page = styled.div`
+width: 400px;
+height: 100vh;
+background-color: #00d27d;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+
+h1{
+    font-family: 'Roboto', sans-serif;
+    font-weight: 700;
+    color: white;
+    font-size: 50px;
+    
+}
+
+@media only screen and (min-width: 200px){
+    max-width: 640px;
+}
+`
+const Forms = styled.div`
+width: 80%;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+input{
+    height: 50px;
+    border-radius: 10px;
+    border-style: none;
+    margin-top: 20px;
+    width: 100%;
+    font-size: 15px;
+    &:focus{
+ outline: 2px solid #21bb57; 
+    }
+}
+button {    
+    margin-top: 20px;
+    height: 40px;
+    width: 101%;
+    border-style: none;
+    border-radius: 5px;
+    background-color: #76de9a;
+    color: white;
+    font-size: 20px;
+    font-weight: 700;
+    cursor : pointer ;
+    
+}
+p {
+color: white;
+margin-top: 15px;
+font-weight: 700;
+cursor : pointer ;
+text-decoration: ${props => props.hover ? 'underline' : 'none'};
+}
+`
